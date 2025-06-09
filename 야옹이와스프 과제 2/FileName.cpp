7a2c5c0bfa0d7d7af5f9b70834c990eec59d074b
t01088101701@gmail.com <t01088101701@gmail.com>
2025-06-09 15:28:22 +0900

    야옹이와 스프 1

---
+++ 야옹이와스프 과제 2/FileName.cpp
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+
+#define ROOM_WIDTH 5
+#define HME_POS    1
+#define BWL_PO     (ROOM_WIDTH - 2)
+
+int main(void) {
+    printf("****야옹이와 수프****\n");
+    printf("      /\\_/\\ \n");
+    printf(" /\\  / o o \\ \n");
+    printf("//\\\\ \\~(*)~/\n");
+    printf("`  \\/     /\n");
+    printf("   | \\|| ||\n");
+    printf("   \\ '|| ||\n");
+    printf("    \\)()-())\n");
+
+    char str[100];
+    printf("야옹이의 이름을 지어 주세요: ");
+    scanf_s("%s", str, 100);
+    printf("야옹이의 이름은 %s입니다\n", str);
+
+    Sleep(1000);
+    system("cls");
+
+    int a = 0; // 수프 개수
+    int b = 2; // 친밀도
+    int potatosoup = 0, mushroomsoup = 0, broccolisoup = 0;
+
+    while (1) {
+        printf(" ==================== 현재 상태 ===================\n");
+        printf(" 현재까지 만든 수프: %d개\n", a);
+        printf(" 집사와의 관계(0~4): %d\n", b);
+        if (b == 0) printf("  곁에 오는 것조차 싫어합니다.\n");
+        else if (b == 1) printf("  간식 자판기 취급입니다.\n");
+        else if (b == 2) printf("  그럭저럭 쓸 만한 집사입니다.\n");
+        else if (b == 3) printf("  훌륭한 집사로 인정 받고 있습니다.\n");
+        else if (b == 4) printf("  집사 껌딱지 입니다.\n");
+        printf(" ==================================================\n");
+
+        Sleep(500);
+
+        int d = 0; // 주사위
+        int soup = rand() % 3;
+
+        printf("\n%s 이동: 집사와 친밀할수록 냄비 쪽으로 갈 확률이 높아집니다.\n", str);
+        printf(" 주사위 눈이 3 이상이면 냄비쪽으로 이동합니다.\n");
+        srand((unsigned int)time(NULL));
+
+        d = rand() % 6 + 1;
+        printf(" 주사위를 굴립니다. 또르륵...\n");
+        printf(" %d이(가) 나왔습니다!\n", d);
+
+        if (d >= 6 - b) {
+            printf("냄비 쪽으로 이동합니다.\n");
+            printf("%s이(가) ", str);
+
+            switch (soup) {
+            case 0:
+                printf("감자수프");
+                potatosoup++;
+                break;
+            case 1:
+                printf("양송이수프");
+                mushroomsoup++;
+                break;
+            case 2:
+                printf("브로콜리수프");
+                broccolisoup++;
+                break;
+            }
+            printf("를 만들었습니다!\n");
+            a++;
+        }
+        else {
+            printf("집 쪽으로 이동합니다.\n");
+        }
+
+        printf("현재까지 만든 수프: %d개 (감자:%d, 양송이:%d, 브로콜리:%d)\n", a, potatosoup, mushroomsoup, broccolisoup);
+
+        printf("\n #####\n");
+        printf(" #H B#\n");
+        printf(" #C  #\n");
+        printf(" #####\n");
+
+        Sleep(500);
+
+        int c = 0;
+        while (1) {
+            printf("\n 어떤 상호작용을 하시겠습니까?  0. 아무것도 하지 않음 1. 긁어 주기\n >> ");
+            scanf_s("%d", &c);
+            if (c == 0 || c == 1) break;
+            else printf("다시 입력해 주세요 (0 또는 1)\n");
+        }
+
+        if (c == 0) {
+            printf(" 아무것도 하지 않습니다.\n");
+            printf(" 4/6의 확률로 친밀도가 떨어집니다.\n");
+
+            d = rand() % 6 + 1;
+            printf(" 주사위를 굴립니다. 또르륵...\n");
+            printf(" %d이(가) 나왔습니다!\n", d);
+            if (d <= 4) {
+                printf(" 친밀도가 떨어집니다.\n");
+                if (b > 0) b--;
+            }
+            else {
+                printf(" 다행히 친밀도가 떨어지지 않았습니다.\n");
+            }
+        }
+        else if (c == 1) {
+            printf(" %s의 턱을 긁어주었습니다.\n", str);
+            printf(" 2/6의 확률로 친밀도가 높아집니다.\n");
+
+            d = rand() % 6 + 1;
+            printf(" 주사위를 굴립니다. 또르륵...\n");
+            printf(" %d이(가) 나왔습니다!\n", d);
+            if (d >= 5) {
+                printf(" 친밀도가 높아집니다.\n");
+                if (b < 4) b++;
+            }
+            else {
+                printf(" 친밀도는 그대로입니다.\n");
+            }
+        }
+
+        printf(" 현재 친밀도: %d\n", b);
+
+        Sleep(2500);
+        system("cls");
+    }
+
+    return 0;
+}
